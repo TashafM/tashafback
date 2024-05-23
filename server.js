4b2d9425790d45449635f6db255f45b931014560
TashafM <mahmood.tashaf@gmail.com>
2024-05-23 13:17:29 +0530

    posts added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDb } from "./config/db.js";
 import { User } from "./models/userModel.js";
+import { Post } from "./models/postModel.js";
 
 const app = express();
 
@@ -45,10 +46,32 @@ app.get("/get-users", async (req, res) => {
   });
 });
 
-app.get("/dummy",(req,res)=>{
-  res.send("Hello Dummy World")
-})
+app.post("/add-post", async (req, res) => {
+  const { title, description } = req.body;
+
+  const post = await Post.create({
+    title,
+    description,
+  });
+
+  await post.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Post added successfully",
+  });
+});
+
+app.get("/get-posts", async (req, res) => {
+  const posts = await Post.find({});
+
+  res.status(200).json({
+    success: true,
+    message: "Posts fetch successfully!",
+    data: posts,
+  });
+});
 
 app.listen(process.env.PORT, () => {
-  console.log("app running "+process.env.PORT);
+  console.log("app running " + process.env.PORT);
 });
